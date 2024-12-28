83cf899c711babace0ff619a21095f976a453f21
MerelDej <merel.dejaegere@student.vives.be>
2024-12-28 15:31:03 +0100

    Report generation file
    File with the basics on how to make a report of the database.

---
+++ report.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import sqlite3
+import os
+from settings.config import DB_PATH
+
+def generate_report(file_type="csv"):
+    conn = sqlite3.connect(DB_PATH)
+    query = """
+    SELECT users.name AS User, orders.item AS Item, orders.price AS Price
+    FROM users
+    JOIN orders ON users.id = orders.user_id
+    """
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+
+    try:
+        os.mkdir("reports")
+        print("Directory 'reports' created successfully.")
+    except FileExistsError:
+        print("Directory 'reports' already exsists.")
+
+    if file_type == "csv":
+        df.to_csv("reports/report.csv", index=False)
+        print("Report generated: reports/report.csv")
+    elif file_type == "excel":
+        df.to_excel("reports/report.xlsx", index=False)
+        print("Report generated: reports/report.xlsx")
